19f0249412072d8e83adc085cfe5269197796b6b
luis lindonjonson <luizlindonjonson@yahoo.com.br>
2023-10-14 00:08:15 -0300

    criando uma pesquisa por usuario

--- src/main/java/br/com/rockset/todolist/user/UserController.java
+++ src/main/java/br/com/rockset/todolist/user/UserController.java
@@ -1,6 +1,8 @@
 package br.com.rockset.todolist.user;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -20,8 +22,12 @@ public class UserController {
     private IUserRepository userRepository;
 
     @PostMapping("/")
-    public UserModel create(@RequestBody UserModel userModel){
+    public ResponseEntity create(@RequestBody UserModel userModel){
+        var user = this.userRepository.findByUserName(userModel.getUserName());
+        if(user != null){
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Usuario já existe");
+        }
        var userCreated = this.userRepository.save(userModel);
-       return userCreated;
+       return ResponseEntity.status(HttpStatus.CREATED).body(userCreated);
     }
 }
