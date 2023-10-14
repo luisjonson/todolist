6bb7e2dfbfc2253c0b8a18d669a6ea807658407f
luis lindonjonson <luizlindonjonson@yahoo.com.br>
2023-10-13 23:27:02 -0300

    aula dois

---
+++ src/main/java/br/com/rockset/todolist/user/UserController.java
@@ -0,0 +1,22 @@
+package br.com.rockset.todolist.user;
+
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * Modificador
+ * public 
+ * private
+ * protected
+ */
+
+@RestController
+@RequestMapping("/users")
+public class UserController {
+    @PostMapping("/")
+    public void create(@RequestBody UserModel userModel){
+        System.out.println(userModel.getName());
+    }
+}
---
+++ src/main/java/br/com/rockset/todolist/user/UserModel.java
@@ -0,0 +1,10 @@
+package br.com.rockset.todolist.user;
+
+import lombok.Data;
+
+@Data
+public class UserModel {
+    private String userName;
+    private String name;
+    private String password;
+}
