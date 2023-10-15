893756055cc3a0b54333d0760cfa68d0502cfa5a
luis lindonjonson <luizlindonjonson@yahoo.com.br>
2023-10-15 10:46:30 -0300

    criando o metodo de update

--- src/main/java/br/com/rockset/todolist/filter/FilterTaskAuth.java
+++ src/main/java/br/com/rockset/todolist/filter/FilterTaskAuth.java
@@ -24,7 +24,7 @@ public class FilterTaskAuth extends OncePerRequestFilter{
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain)
             throws ServletException, IOException {
            var servletPath = request.getServletPath();
-        if(servletPath.equals("/tasks/")){
+        if(servletPath.startsWithyy("/tasks/")){
 
             //Pegar a autenticação (usuario senha)    
             var authorization = request.getHeader("Authorization");
